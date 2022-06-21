49c19cf29f11b1a29522d54b132a25c8f67b577e
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-06-21 16:14:12 +0200

    Clinic to 85%

---
+++ src/vetClinic/Clinic.java
@@ -0,0 +1,62 @@
+package vetClinic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clinic {
+    private final int capacity;
+    private final List<Pet> data;
+
+    public Clinic(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Pet pet){
+        if (this.data.size()<this.capacity){
+            this.data.add(pet);
+        }
+    }
+
+    public boolean remove(String name){
+        for (Pet pet : this.data) {
+            if (pet.getName().equals(name)){
+                return this.data.remove(pet);
+            }
+        }
+        return false;
+    }
+
+    public Pet getPet(String name, String owner){
+        for (Pet pet : this.data) {
+            if (pet.getName().equals(name) && pet.getOwner().equals(owner)){
+                return pet;
+            }
+        }
+        return null;
+    }
+
+    public Pet getOldestPet(){
+        Pet pet = new Pet();
+        for (Pet currPet : this.data) {
+            if (currPet.getAge()>pet.getAge()){
+                pet = currPet;
+            }
+        }
+        return pet;
+    }
+
+    public int getCount(){
+        return this.data.size();
+    }
+
+    public String getStatistics(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("The clinic has the following patients:\n");
+
+        for (Pet pet : this.data) {
+            builder.append(pet.getName()).append(" ").append(pet.getOwner()).append(System.lineSeparator());
+        }
+        return builder.toString();
+    }
+}
